66a67e1c89823f343a0c94c7d4874238ab57d060
zykgithub1 <787950451@qq.com>
2021-02-21 23:51:50 +0800

    字符串

---
+++ python/py_2_21_study.py
@@ -0,0 +1,23 @@
+"""a=-16
+b=-16
+print(a is b)
+a=-5
+b=-5
+print(a is b)
+a=100000000000000000000000
+b=100000000000000000000001
+print(a is b)
+a is b
+a=null
+b=5
+print(a is b)"""
+#字符串
+'''a="abc"
+b="abc"
+print(a==b)
+a='a'
+b='a'
+print(a==b)'''
+ord('A')
+
+
